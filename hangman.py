71e5081cf84e64995e633c9d8112ac727f67be7f
Andrew <shakeyshakespeare0@gmail.com>
2023-07-23 21:43:09 +0100

    Update hangman

--- hangman.py
+++ hangman.py
@@ -2,9 +2,9 @@ from list_of_animals import animal_list
 
 hangman = ["  +---+  ",
            "  |   |  ",
-           "  X   |  ",
-           " XXX  |  ",
-           " X X  |  ",
+           "      |  ",
+           "      |  ",
+           "      |  ",
            "      |  ",
            "========="]
 
@@ -22,9 +22,16 @@ def add_letters_to_guess(word, blanked_word, letter):
     return "".join(blanked_word)
 
 
-def print_hangman():
+def print_hangman(blanked_word):
     print("")
-    for line in hangman:
+    printing = [line for line in hangman]
+    side = [letter for letter in used_letters if letter not in blanked_word]
+    while len(side) < 6:
+        side.append(" ")
+    printing[1] += f"  {side[0]}  {side[1]}"
+    printing[3] += f"  {side[2]}  {side[3]}"
+    printing[5] += f"  {side[4]}  {side[5]}"
+    for line in printing:
         print(line)
 
 
@@ -45,16 +52,19 @@ def update_guess(word, blanked_word, letter):
 
 def get_guess(blanked_word):
     print(f"\nWORD: {blanked_word}")
-    print_hangman()
+    print_hangman(blanked_word)
     while True:
         guess_letter = input("\nWhich letter? ")
         if guess_letter in used_letters:
             print("\nYou've already guessed that letter!")
         elif not guess_letter.isalpha():
             print("\nYou need to enter a letter!")
+        elif len(guess_letter) != 1:
+            print("\nYou must enter a single letter!")
         else:
             used_letters.append(guess_letter.lower())
             break
+    print("\n\n")
     return guess_letter.lower()
 
 
@@ -67,9 +77,9 @@ while len(hangman_set) > 0 and word != blanked_word:
     blanked_word = update_guess(word, blanked_word, letter)
 if word == blanked_word:
     print(f"\nThe word was: {word}! Excellent work!")
-    print_hangman()
+    print_hangman(blanked_word)
     print("\nYou saved our hangman!")
 else:
     print(f"\nThe word was: {word}.")
-    print_hangman()
+    print_hangman(blanked_word)
     print("\nOuch! Poor hangman.")
